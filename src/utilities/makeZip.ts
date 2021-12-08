79bf46fab275b36d2108864d8252173b485ad521
unknown <=>
2021-12-08 11:03:33 +0600

    Heroku

--- src/utilities/makeZip.ts
+++ src/utilities/makeZip.ts
@@ -3,7 +3,7 @@ const zl = require("zip-lib");
 function createZip(src, destFile) {
 	return new Promise((resolve, reject)=>{
 	zl.archiveFolder(src, destFile).then(function () {
-		console.log("done to create zip");
+		console.log("done to create zip.");
 		resolve(true)
 	}, function (err) {
 		reject(err)
