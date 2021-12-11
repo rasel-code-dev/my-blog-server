fc66df63b4a6c2fb3693f195680ec917e2c893c1
rasel-code-dev <raselmr005@gmail.com>
2021-12-11 17:38:39 +0600

    user dashboard control and user can update their profile information v7.0

--- dist/src/logger/errorConsole.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function errorConsole(ex) {
-    if (process.env.NODE_ENV === "development") {
-        console.error(ex);
-    }
-}
-exports.default = errorConsole;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXJyb3JDb25zb2xlLmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsic3JjL2xvZ2dlci9lcnJvckNvbnNvbGUudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxTQUFTLFlBQVksQ0FBQyxFQUFFO0lBQ3RCLElBQUcsT0FBTyxDQUFDLEdBQUcsQ0FBQyxRQUFRLEtBQUssYUFBYSxFQUFDO1FBQ3hDLE9BQU8sQ0FBQyxLQUFLLENBQUMsRUFBRSxDQUFDLENBQUE7S0FDbEI7QUFDSCxDQUFDO0FBRUQsa0JBQWUsWUFBWSxDQUFBIn0=
--- src/database/db.js
+++
@@ -1,6 +0,0 @@
-const low = require('lowdb')
-const FileSync = require("lowdb/adapters/FileSync");
-const db = low(new FileSync("./src/database/db.json"))
-
-
-export default db
