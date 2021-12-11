3011f470ed383a3df9ceaf8b40e731fabbb68285
rasel-code-dev <raselmr005@gmail.com>
2021-12-11 17:44:53 +0600

    user dashboard control and user can update their profile information v7.0

--- src/controllers/index.ts
+++
@@ -1,8 +0,0 @@
-
-
-
-import * as postController  from "./postController"
-import * as authController  from "./authController"
-import * as commentController  from "./commentController"
-import * as filesController  from "./filesController"
-export default { postController, authController, commentController, filesController }
